cc2e690e7d62996399914b777ee19543a551264f
Poseyd29 <poseyd29@gmail.com>
2022-09-12 19:42:01 -0400

    chained fetch returning promise
    chained fetch returning promise but not getting the overview data of each title in the titles array yet

--- public/app.js
+++ public/app.js
@@ -24,58 +24,18 @@ fetch('https://online-movie-database.p.rapidapi.com/title/get-most-popular-tv-sh
         let trimmer = list.map(element => element.slice(7, 17))
 
         let trimmed = trimmer.map(el => el.replaceAll('/', ''))
-        // let trimmed = for (let i = 0; i < trimmer.length)
 
-        // list.map((item) => {
-        //     const name = item.l;
-        //     const poster = item.i.imageUrl;
-        //     const movie = `<li><img src="${poster}"><h2>${name}</h2></li>`
-        //     document.querySelector('.movies').innerHTML += movie;
-        // })
-
+        let titles = []
+        for (let i = 0; i < trimmed.length; i++) {
+            titles.push(fetch(`https://online-movie-database.p.rapidapi.com/title/get-overview-details?tconst=${trimmed[i]}&currentCountry=US`, options))
+            // console.log(titles)
+        }
+        console.log(titles)
     })
-    .then()
+    .then(titles => {
+        console.log(titles)
+    })
     .catch(err => console.error(err));
-
-// fetch('https://jsonplaceholder.typicode.com/posts/5').then(function (response) {
-//     if (response.ok) {
-//         return response.json();
-//     } else {
-//         return Promise.reject(response);
-//     }
-// }).then(function (data) {
-
-//     // Store the post data to a variable
-//     post = data;
-
-//     // Fetch another API
-//     return fetch('https://jsonplaceholder.typicode.com/users/' + data.userId);
-
-// }).then(function (response) {
-//     if (response.ok) {
-//         return response.json();
-//     } else {
-//         return Promise.reject(response);
-//     }
-// }).then(function (userData) {
-//     console.log(post, userData);
-// }).catch(function (error) {
-//     console.warn(error);
-// });
-
-// const optionsTwo = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'Z2EvqnO4xwmsh2eY3rMTIV2ivj5hp1QsuGUjsnrYp69UBS4EI5',
-//         'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-//     }
-// };
-
-// fetch('https://online-movie-database.p.rapidapi.com/title/find?q=tt11198330', optionsTwo)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
 
 
 
